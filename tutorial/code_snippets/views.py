635328b0960eff8a68be7cea7510d347e87d4046
shakyav <shakya.v@northeastern.edu>
2021-05-07 15:31:43 -0400

    readme update , comments added for serializers.py and views.py

--- tutorial/code_snippets/views.py
+++ tutorial/code_snippets/views.py
@@ -10,7 +10,9 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-
+""" below class creates an api with all possible HTTP methods 
+i.e. GET, POST, PUT and Delete for snippets model by importing the 
+generics.RetrieveUpdateDestroyAPIView from django's rest_framework"""
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
